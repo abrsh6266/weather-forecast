2bb26cc9e2b7148cb3c465567c0c94475f847233
abrsh6266 <abrsh6266@gmail.com>
2023-03-16 15:26:42 +0900

    removing unneccessary files

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('client side javascript')
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
